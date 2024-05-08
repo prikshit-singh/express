0477ae640d2019ec5167afc70811db5c9d89c1cc
prikshit <43513459+prikshit-singh@users.noreply.github.com>
2024-05-08 15:35:44 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 require('dotenv').config();
 const express = require('express')
-
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
 const connectDB = require('./config/dbconfig/dbconfig')
 const app = express()
+
+
+app.use(express.static('./files'));
+app.use(express.static('./uploads'));
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 connectDB()
 
 
