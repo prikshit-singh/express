ccab8c883fe3e0741a29b1a6ce1ff107ad92300f
prikshit <43513459+prikshit-singh@users.noreply.github.com>
2024-05-08 15:14:35 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,18 +4,17 @@ const express = require('express')
 
 const connectDB = require('./config/dbconfig/dbconfig')
 const app = express()
-const bodyParser = require('body-parser');
-const path = require('path');
-const fs = require('fs');
+// const bodyParser = require('body-parser');
+// const path = require('path');
+// const fs = require('fs');
 
-app.use(express.static('./files'));
-app.use(express.static('./uploads'));
-//app.use(cors())
-// app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
-connectDB()
+// app.use(express.static('./files'));
+// app.use(express.static('./uploads'));
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.json({ limit: '50mb' }));
+// app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+// connectDB()
 
 
 
