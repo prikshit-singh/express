37c36f1fda0226a770c53864c0e5e165d4dca725
prikshit <43513459+prikshit-singh@users.noreply.github.com>
2024-05-08 16:54:42 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
 
-const connectDB = require('./config/dbconfig/dbconfig')
+const connectDB = require('../config/dbconfig/dbconfig')
 const app = express()
 const bodyParser = require('body-parser');
 
