ab036a0954081e1401981895ac3284c36968dc87
prikshit <43513459+prikshit-singh@users.noreply.github.com>
2024-05-08 15:18:20 +0530

    Add files via upload

---
+++ api/config/dbconfig/dbconfig.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+module.exports=async function connectDB() {
+  try {
+    if (mongoose.connection.readyState !== 1) {
+      await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+    }
+    const db = mongoose.connection;
+   
+
+    db.on('error', (error) => {
+      console.error('MongoDB connection error:', error);
+    });
+
+    if (db.readyState === 1) {
+      console.log('Connected to MongoDB');
+    } else {
+      db.once('open', () => {
+        console.log('Connected to MongoDB');
+      });
+    }
+
+    console.log('Connection process completed.');
+
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw new Error('Failed to connect to MongoDB');
+  }
+}
